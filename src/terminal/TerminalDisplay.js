df94e976fa2fb453c1585f8133b00bc4d8c400b3
Roni Scott <scot0410@d.umn.edu>
2022-07-12 19:47:27 -0500

    Initial commit - including: create-react-app setup with react-console-emulator

---
+++ src/terminal/TerminalDisplay.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import Terminal from 'react-console-emulator'
+
+const commands = {
+  echo: {
+    description: 'Echo a passed string.',
+    usage: 'echo <string>',
+    fn: (...args) => args.join(' ')
+  }
+}
+
+export default class MyTerminal extends Component {
+  render () {
+    return (
+      <Terminal
+        commands={commands}
+        welcomeMessage={'Welcome to the React terminal!'}
+        promptLabel={'me@React:~$'}
+      />
+    )
+  }
+}
